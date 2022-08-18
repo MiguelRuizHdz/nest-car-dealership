008e60e33d769937643eecfc3d23be85972c0682
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-08-17 20:51:48 -0500

    Pipe - ParseUUIDPipe

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post, Put } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -15,7 +15,7 @@ export class CarsController {
     }
 
     @Get( ':id' )
-    getCarById( @Param('id') id: string ) {
+    getCarById( @Param('id', ParseUUIDPipe ) id: string ) {
         console.log({ id });
         return this.carsService.findOneById(id);
     }
