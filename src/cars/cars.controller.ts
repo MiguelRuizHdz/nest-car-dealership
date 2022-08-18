b225856ee5d77c9488d54f341f1834b91c35b29d
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-08-18 07:31:52 -0500

    DTO - Data Transfer Object

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post, Put } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -21,8 +22,8 @@ export class CarsController {
     }
 
     @Post()
-    createCar( @Body() req: any) {
-        return req;
+    createCar( @Body() createCarDto: CreateCarDto) {
+        return createCarDto;
     }
 
     @Patch( ':id' )
